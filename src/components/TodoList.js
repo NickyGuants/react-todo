567cdcb7cb7e78ee9e235ed62df428dd086f8c5a
NickyGuants <nicholasguantai528@gmail.com>
2021-10-25 10:09:58 +0300

    create the todo lis

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = ()=>{
+    return (
+        <div className="TodoList">
+            <ul>
+                <li><Todo /></li>
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
