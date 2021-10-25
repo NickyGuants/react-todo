f3cdc4ef13de83383686ae3b6558330b38d45bd7
NickyGuants <nicholasguantai528@gmail.com>
2021-10-25 16:38:01 +0300

    use counter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import '../App.css';
+import Counter from "./Counter";
 
 const Header = () =>{
     return (
         <header>
             <h1>My Todo List</h1>
+            <Counter />
         </header>
     );
 }
