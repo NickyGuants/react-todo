13a3da2aa09aca7706602f000b0f611d1dee790d
NickyGuants <nicholasguantai528@gmail.com>
2021-10-25 16:38:18 +0300

    add todo form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+import { useDispatch } from "react-redux";
 import '../App.css';
 
-const Form = () =>{
+function Form () {
+    const [title, setTitle] =useState("");
+    const dispatch = useDispatch();
+
+    const todoItem={
+        id: Math.floor(Math.random()*100000),
+        title,
+    }
+
+    const addTodo= e =>{
+        e.preventDefault();
+        dispatch(addTodo(todoItem));
+    }
+
     return (
         <div className="form-container">
             <div className="form">
-                <input type="text" placeholder="Add todo..." />
-                <button>Submit</button>
+                <form onSubmit={addTodo}>
+                <input type="text" 
+                placeholder="Add todo..."  
+                value={title}
+                onChange={(e) => setTitle(e.target.value)} 
+                />
+                <button onClick={addTodo}>Submit</button>
+                </form>
             </div>
         </div>
     );
