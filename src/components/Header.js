9e77a36cd3b311b179d98eb97eac5ac4ffd39967
NickyGuants <nicholasguantai528@gmail.com>
2021-10-26 07:45:01 +0300

    remove counter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import React from "react";
 import '../App.css';
-import Counter from "./Counter";
 
 const Header = () =>{
     return (
         <header>
             <h1>My Todo List</h1>
-            <Counter />
         </header>
     );
 }
