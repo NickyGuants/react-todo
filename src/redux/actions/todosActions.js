1c22e93b73a143e5de95dfa10b6048997a07540a
NickyGuants <nicholasguantai528@gmail.com>
2021-10-26 07:43:54 +0300

    add completion

--- src/redux/actions/todosActions.js
+++ src/redux/actions/todosActions.js
@@ -1,18 +1,25 @@
-import Todo from '../../components/Todo'
-
 import {ADD_TODO, REMOVE_TODO} from '../types'
 
 
-export const addTodo = () =>{
+export const addTodo = (todo) =>{
     return {
         type: ADD_TODO,
-        payload: Todo
-    }
-}
+        payload: todo,
+    };
+};
 
-export const subtractCounter =() =>{
+
+export const removeTodo = (id) =>{
     return {
         type: REMOVE_TODO,
-        payload: 1
-    }
-}
+        payload: id,
+    };
+};
+
+export const markTodoAsComplete = (id) => {
+    return {
+      //type: MARK_TODO_COMPLETE,
+      payload: id,
+    };
+  };
+  
